041e97255c70833eb2cdd61874c811d2048c94ed
footzi <footzi@mail.ru>
2017-07-03 07:05:49 +0300

    complite price

--- source/js/common.js
+++ source/js/common.js
@@ -1,62 +1,61 @@
-// $(document).ready(function(){
+$(document).ready(function() {
 
-// 	$("#governance, #creative, #graphic, #game, #full").hide();
-	
-// 	$(".icon").on("click", function() {
-// 		var select=$(this).attr("data-link");
-// 		if ((select == "#customer")||(select == "#governance")||(select == "#creative")) {
-// 			$(".about-details-icons .icon").removeClass("icon-active");
-// 			$(this).addClass("icon-active");
-// 			$(".about-details-content").hide()
-// 		} else {
-// 			$(".services-content-icon .icon").removeClass("icon-active");
-// 			$(this).addClass("icon-active");
-// 			$(".services-content-item").hide()
-// 		}
-// 		$(select).fadeIn();
-// 	})
+    $("#governance, #creative, #graphic, #game, #full").hide();
 
-
-// 	$(".portfolio-nav a").on("click", function() {
-// 		var link=$(this).attr("data-filter")
-
-// 		if (link==".all") {
-// 			$(".portfolio-products-item").fadeIn()
-// 			$(".portfolio-link").removeClass("is-checked")
-// 			$(".portfolio-products-column").removeClass("flex-row")
-		
-// 			$(this).addClass("is-checked")
-
-// 		} else {
-// 			$(".portfolio-products-item").hide()
-// 			$(".portfolio-link").removeClass("is-checked")
-// 			$(".portfolio-products-column").addClass("flex-row")
-			
-// 			$(this).addClass("is-checked")
-// 			$(link).fadeIn()	
-
-			
-// 		}
-// 	})
-
-// 	setInterval(function() {
-// 		$(".bar").each(function () {
-// 			var widthP = $(this).parent().width();
-// 			var width = Math.round($(this).width()/widthP*100)+"%";
-// 			var percent = $(this).parent().siblings(".progress-title").children(".progress-percent");
-// 			percent.empty().append(width);
-// 		})	
-// 	},200000);
+    $(".icon").on("click", function() {
+        var select = $(this).attr("data-link");
+        if ((select == "#customer") || (select == "#governance") || (select == "#creative")) {
+            $(".about-details-icons .icon").removeClass("icon-active");
+            $(this).addClass("icon-active");
+            $(".about-details-content").hide()
+        } else {
+            $(".services-content-icon .icon").removeClass("icon-active");
+            $(this).addClass("icon-active");
+            $(".services-content-item").hide()
+        }
+        $(select).fadeIn();
+    })
 
 
-// 	$(".second-text").hide();
-// 	$(".btn-open").on("click", function() {
-// 		$(this).hide();
-// 		$(this).parent().siblings(".second-text").fadeIn();
-// 	})
-// 	$(".btn-close").on("click", function() {
-// 		$(this).parent().fadeOut();
-// 		$(".btn-open").show()
-// 	})
-// });
+    $(".portfolio-nav a").on("click", function() {
+        var link = $(this).attr("data-filter")
 
+        if (link == ".all") {
+            $(".portfolio-products-item").fadeIn()
+            $(".portfolio-link").removeClass("is-checked")
+            $(".portfolio-products-column").removeClass("flex-row")
+
+            $(this).addClass("is-checked")
+
+        } else {
+            $(".portfolio-products-item").hide()
+            $(".portfolio-link").removeClass("is-checked")
+            $(".portfolio-products-column").addClass("flex-row")
+
+            $(this).addClass("is-checked")
+            $(link).fadeIn()
+
+
+        }
+    })
+
+    setInterval(function() {
+        $(".bar").each(function() {
+            var widthP = $(this).parent().width();
+            var width = Math.round($(this).width() / widthP * 100) + "%";
+            var percent = $(this).parent().siblings(".progress-title").children(".progress-percent");
+            percent.empty().append(width);
+        })
+    }, 200000);
+
+
+    $(".second-text").hide();
+    $(".btn-open").on("click", function() {
+        $(this).hide();
+        $(this).parent().siblings(".second-text").fadeIn();
+    })
+    $(".btn-close").on("click", function() {
+        $(this).parent().fadeOut();
+        $(".btn-open").show()
+    })
+});
